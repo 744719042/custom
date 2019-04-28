2044c42040f8c596256767d1c8e9a7e57d3ceaf1
xingzhongzhang <xingzhongzhang@sohu-inc.com>
2019-04-28 09:39:13 +0800

    去掉回弹底部的ListView，防止下拉卡顿

--- app/src/main/java/com/example/custom/ZoomHeaderActivity.java
+++ app/src/main/java/com/example/custom/ZoomHeaderActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import com.example.custom.adapter.UserListAdapter;
-
 public class ZoomHeaderActivity extends AppCompatActivity {
     private ListView listView;
 
@@ -13,7 +11,7 @@ public class ZoomHeaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zoom_header);
-        listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(new UserListAdapter(this));
+//        listView = (ListView) findViewById(R.id.listView);
+//        listView.setAdapter(new UserListAdapter(this));
     }
 }
